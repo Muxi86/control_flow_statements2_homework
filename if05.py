7c6c926ce366c2194fa4678b9553087b4a353d46
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2022-12-31 19:30:47 +0500

    i05

--- if05.py
+++ if05.py
@@ -6,4 +6,37 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a = n//pow(10,4)
+    b = n%pow(10,4)//pow(10,3)
+    c = n%pow(10,4)%pow(10,3)//pow(10,2)
+    d = n%pow(10,4)%pow(10,3)%pow(10,2)//10
+    e = n%10
+
+    if a > b:
+        if a > c:
+            if a > d:
+                if a > e:
+                    ans = a
+    if b > a:
+        if b > c:
+            if b > d:
+                if b > e:
+                    ans = b
+    if c > a:
+        if c > b:
+            if c > d:
+                if c > e:
+                    ans = c
+    if d > a:
+        if d > b:
+            if d > c:
+                if d > e:
+                    ans = d
+    if e > a:
+        if e > b:
+            if e > c:
+                if e > d:
+                    ans = e    
+    return ans
+
+print(main(43176))
